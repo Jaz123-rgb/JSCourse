56c21a6a173210b2e394950d2e1a10f2824d3d2a
jaz123-rgb <pjaziel723@gmail.com>
2022-09-20 13:38:13 -0500

    add changes to app express

--- nodecourse/05_express/app.js
+++ nodecourse/05_express/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+
+const users = [
+    {id:1, name:"Jaz"},
+    {id:2, name:"Luis"},
+    {id:3, name:"Marth"}
+
+];
+
+
 app.get('/', (req, res) => {
     res.send("im a test");
 });
@@ -10,8 +19,11 @@ app.get('api/users', (req, res)=>{
     res.send(['Grover', 'Luis', 'Ana']);
 });
 
-app.get('api/users/:year/:mon', (req, res)=>{
-    res.send(req.query);
+//Send a id to prove what tyoe of http req use 
+app.get('api/users/:id', (req, res)=>{
+   let user = users.find(u => u.id === parseInt(req.params.id)); 
+   if(users) res.status(404).send('the user can be chatch');
+    res.send()
 });
 
 
