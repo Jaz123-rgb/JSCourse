a9545176b2e4135f53df087e50a312324107e13b
Jaz <pjaziel723@gmail.com>
2022-09-21 03:55:38 -0500

    Route to test get and post

--- nodecourse/05_express/app.js
+++ nodecourse/05_express/app.js
@@ -1,11 +1,17 @@
+
+const {json} = require('express');
 const express = require('express');
+const Joi = require('@hapi/joi');
+const {error} = require('@hapi/joi/lib/annotate');
 const app = express();
 
 
+app.use(express.json());
+
 const users = [
-    {id:1, name:"Jaz"},
-    {id:2, name:"Luis"},
-    {id:3, name:"Marth"}
+    {id:1, name:'Jaz'},
+    {id:2, name:'Luis'},
+    {id:3, name:'Marth'}
  
 ];
 
@@ -15,16 +21,55 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('api/users', (req, res)=>{
+app.get('/api/users', (req, res)=>{
     res.send(['Grover', 'Luis', 'Ana']);
 });
 
 //Send a id to prove what tyoe of http req use 
-app.get('api/users/:id', (req, res)=>{
-   let user = user.find(u => u.id === parseInt(req.params.id)); 
+app.get('/api/users/:id', (req, res)=>{
+   let user = users.find(u => u.id === parseInt(req.params.id)); 
    if(!user) res.status(404).send('the user can be chatch');
     res.send(user);
 });
+
+app.post('/api/users/', (req, res)=>{
+     //ADD joi in to validate info
+
+
+    const schema = Joi.object({
+	name: Joi.string().min(3).required()
+    });
+   const {error, value}  =  schema.validate({name: req.body.name});
+    if(!error){
+	const user = {
+	    id: users.length + 1,
+	    name: value.name
+	};
+	users.push(user);
+	res.send(user);
+    }else{
+	const notmesg = error.details[0].message;
+	res.status(400).send(notmesg);
+
+    }
+
+    //ADD joi in to validate info
+
+//create a auth easy
+//    if (!req.body.name || req.body.name.length <= 2){
+	//400 Bad Request
+  //   res.status(400).send('Add a name, with a 3 letters like minus');
+//	return;
+    //}
+//create a auth easy 
+  //  const user = {
+//	id: users.length + 1, 
+//	name: req.body.name
+    //}
+   // users.push(user);
+    // res.send(user);
+});
+
 
 
 const port = process.env.PORT || 3000;
